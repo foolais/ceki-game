2d6872555c9362e2af39b387c153f667ed6d5b80
Wahyu Esya <wahyu.esya17@gmail.com>
2024-07-12 01:33:05 +0700

    feat:add home page

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,14 +3,14 @@ import { Container, Box } from "@chakra-ui/react";
 
 const MainLayout = ({ children }) => {
   return (
-    <Box w="100vw" h="100vh">
+    <Box w="100vw" h="100vh" bg="#1e1e1e">
       <Container
         w={{ base: "100%", md: "container.sm" }}
         h="100vh"
-        bg="#1E1E1E"
+        bg="white"
         centerContent
       >
-        <Box w="100%" p={4} color="#7ECA9C">
+        <Box w="100%" p={4} color="teal">
           {children}
         </Box>
       </Container>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,50 @@
-import MainLayout from "../components/Layouts/MainLayout";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import MainLayout from "../components/MainLayout";
+import cardGame from "../assets/cardGame.lottie";
+import { Heading, Box, Button, Stack } from "@chakra-ui/react";
+
+const lottieBoxStyle = {
+  maxWidth: "400px",
+  minHeight: "225px",
+  overflow: "hidden",
+};
 
 const HomePage = () => {
   return (
     <MainLayout>
-      <h1>Hello</h1>
+      <Box
+        h="80vh"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        gap={4}
+      >
+        <Heading as="h1" size="3xl" textAlign="center">
+          CEKI Game
+        </Heading>
+        <Box style={lottieBoxStyle}>
+          <DotLottieReact
+            src={cardGame}
+            loop
+            autoplay
+            mode="bounce"
+            style={{
+              width: "100%",
+              height: "100%",
+              scale: "2",
+            }}
+          />
+        </Box>
+        <Stack direction="column" align="center" spacing={4} mt={6}>
+          <Button size="md" colorScheme="teal" w="250px">
+            Create New Game
+          </Button>
+          <Button size="md" colorScheme="teal" variant="outline" w="250px">
+            Load Game
+          </Button>
+        </Stack>
+      </Box>
     </MainLayout>
   );
 };
