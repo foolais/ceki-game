c21f0269311bc8efc6d1a0e77bf8d1a4849527f3
Wahyu Esya <wahyu.esya17@gmail.com>
2024-07-12 22:47:51 +0700

    fix: homepage layout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,12 +3,17 @@ import { Container, Box } from "@chakra-ui/react";
 
 const MainLayout = ({ children }) => {
   return (
-    <Box w="100vw" h="100vh" bg="#1e1e1e">
+    <Box w="100vw" h={{ base: "90vh", md: "100vh" }} bg="#1e1e1e">
       <Container
         w={{ base: "100%", md: "container.sm" }}
-        h="100vh"
+        h="100%"
         bg="white"
         centerContent
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        gap={4}
       >
         <Box w="100%" p={4} color="teal">
           {children}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,48 +3,39 @@ import MainLayout from "../components/MainLayout";
 import cardGame from "../assets/cardGame.lottie";
 import { Heading, Box, Button, Stack } from "@chakra-ui/react";
 
-const lottieBoxStyle = {
-  maxWidth: "400px",
-  minHeight: "225px",
-  overflow: "hidden",
-};
-
 const HomePage = () => {
   return (
     <MainLayout>
+      <Heading as="h1" size="3xl" textAlign="center">
+        CEKI Game
+      </Heading>
       <Box
-        h="80vh"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        gap={4}
+        h={{ base: "250px", md: "350px" }}
+        style={{
+          maxWidth: "100%",
+          overflow: "hidden",
+        }}
       >
-        <Heading as="h1" size="3xl" textAlign="center">
-          CEKI Game
-        </Heading>
-        <Box style={lottieBoxStyle}>
-          <DotLottieReact
-            src={cardGame}
-            loop
-            autoplay
-            mode="bounce"
-            style={{
-              width: "100%",
-              height: "100%",
-              scale: "2",
-            }}
-          />
-        </Box>
-        <Stack direction="column" align="center" spacing={4} mt={6}>
-          <Button size="md" colorScheme="teal" w="250px">
-            Create New Game
-          </Button>
-          <Button size="md" colorScheme="teal" variant="outline" w="250px">
-            Load Game
-          </Button>
-        </Stack>
+        <DotLottieReact
+          src={cardGame}
+          loop
+          autoplay
+          mode="bounce"
+          style={{
+            width: "100%",
+            height: "100%",
+            scale: "2",
+          }}
+        />
       </Box>
+      <Stack direction="column" align="center" spacing={4} mt={6}>
+        <Button size="md" colorScheme="teal" w="250px">
+          Create New Game
+        </Button>
+        <Button size="md" colorScheme="teal" variant="outline" w="250px">
+          Load Game
+        </Button>
+      </Stack>
     </MainLayout>
   );
 };
